21c9c1777336a9c678e52922be1f1bda349dd1d0
ryaningham2001 <ryaningham2001@gmail.com>
2023-03-14 02:07:03 +0000

    restructure to class

--- classifier_keras.py
+++ classifier_keras.py
@@ -6,6 +6,7 @@ import keras
 from keras.models import Sequential
 from keras.layers import Dense
 from keras import optimizers
+from keras import callbacks
 from keras.callbacks import TensorBoard
 from keras.utils import plot_model
 import tensorflow as tf
@@ -13,78 +14,131 @@ import datetime
 import time
 import matplotlib.pyplot as plt
 import os
+from kerastuner import BayesianOptimization
 
-def load_data(filepath):
-    df = pd.read_csv(filepath, na_values="?")
-    df.dropna(inplace=True)
-    df.drop(columns=['car name'], inplace=True)
-    return df
+class Keras_NN:
+    def __init__(self, filepath, target_column, categorical_features=[]):
+        self.filepath = filepath
+        self.target_column = target_column
+        self.categorical_features = categorical_features
+        self.df = None
+        self.X_train = None
+        self.X_test = None
+        self.y_train = None
+        self.y_test = None
+        self.model = None
+        self.history = None
+        self.test_loss = None
+        self.tensorboard = TensorBoard(log_dir='./logs', histogram_freq=0, write_graph=True, write_images=True)
+    
+    def load_data(self):
+        df = pd.read_csv(self.filepath, na_values="?")
+        df.dropna(inplace=True)
+        df.drop(columns=['car name'], inplace=True)
+        self.df = df
+    
+    def one_hot_encode(self):
+        self.df = pd.get_dummies(self.df, columns=self.categorical_features)
+    
+    def split_data(self):
+        features = self.df.drop(self.target_column, axis=1)
+        target = self.df[self.target_column]
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(features, target, test_size=0.2, random_state=0)
+    
+    def standardise_data(self):
+        scaler = StandardScaler()
+        self.X_train = scaler.fit_transform(self.X_train)
+        self.X_test = scaler.transform(self.X_test)
+    
+    def build_model(self, hp):
+        """
+        In this function, we are defining a Keras Sequential model with the input shape and hidden layers that will be searched by Keras Tuner using hyperparameters.
+        The hp object is the hyperparameter object that is passed by Keras Tuner to this function.
 
-def one_hot_encode(df, categorical_features):
-    return pd.get_dummies(df, columns=categorical_features)
+        We are defining two types of hyperparameters here:
+            Integer hyperparameters: hp.Int. This hyperparameter is used to specify the number of units in a layer. The minimum value is 32, the maximum is 256, and the step size is 32.
+            Choice hyperparameters: hp.Choice. This hyperparameter is used to specify the activation function for a layer and learning rate for optimizer.
+        
+        After defining the model architecture, we compile it with the Adam optimizer and mean squared error loss function."""
+        
+        model = Sequential()
+        model.add(Dense(units=hp.Int('units_input', min_value=32, max_value=256, step=32),
+                        activation=hp.Choice('act_input', values=['relu', 'sigmoid']),
+                        input_shape=(self.X_train.shape[1],)))
+        
+        for i in range(hp.Int('num_layers', 1, 5)):
+            model.add(Dense(units=hp.Int('units_' + str(i), min_value=32, max_value=256, step=32),
+                            activation=hp.Choice('act_' + str(i), values=['relu', 'sigmoid'])))
+        
+        model.add(Dense(1, activation='linear'))
+        model.compile(optimizer=optimizers.Adam(learning_rate=hp.Choice('learning_rate', values=[1e-2, 1e-3])),
+                    loss='mean_squared_error', metrics=['accuracy'])
+        
+        self.model = model
 
-def split_data(df, target_column):
-    features = df.drop(target_column, axis=1)
-    target = df[target_column]
-    X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=0)
-    return X_train, X_test, y_train, y_test
+    # def tune_hyperparameters(self):
+    #     param_grid = {
+    #         'hidden_layer_size': [8, 16, 32],
+    #         'learning_rate': [0.01, 0.001, 0.0001],
+    #     }
+    #     model = KerasRegressor(build_fn=self.build_model, epochs=40, batch_size=32, verbose=0)
+    #     self.grid_search = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=-1, cv=3)
+    #     self.grid_search.fit(self.X_train, self.y_train)
+    #     print("Best hyperparameters found:", self.grid_search.best_params_)
 
-def standardise_data(X_train, X_test):
-    scaler = StandardScaler()
-    X_train = scaler.fit_transform(X_train)
-    X_test = scaler.transform(X_test)
-    return X_train, X_test
+    def train_model(self):
+        self.history = self.model.fit(self.X_train, self.y_train, epochs=40, batch_size=32, callbacks=[self.tensorboard], verbose=0)
+    
+    def evaluate_model(self):
+        self.test_loss = self.model.evaluate(self.X_test, self.y_test, verbose=0)
+    
+    def predict(self, df):
+        return self.model.predict(df)
+    
+    def plot_history(self):
+        plt.plot(self.history.history['loss'])
+        plt.plot(self.history.history['accuracy'])
+        plt.title('Model Loss and Accuracy')
+        plt.ylabel('Loss / Accuracy')
+        plt.xlabel('Epoch')
+        plt.legend(['Loss', 'Accuracy'], loc='upper right')
+        plt.show()
+    
+    def save_model(self):
+        if not os.path.exists('models_qa'):
+            os.makedirs('models_qa')
+        time_str = datetime.datetime.now().strftime('%Y_%m_%d_%H_%M')
+        self.model.save(f"models_qa/model_keras_latest.h5")
+    
+    def load_model(self, model_file):
+        self.model = keras.models.load_model(model_file)
+    
+    def run(self):
+        self.load_data()
+        self.one_hot_encode()
+        self.split_data()
+        self.standardise_data()
+        #self.tune_hyperparameters()
+        #best_params = self.grid_search.best_params_
+        
+        tuner = BayesianOptimization(
+            self.build_model,
+            objective='val_loss',
+            max_trials=5,
+            executions_per_trial=3,
+            directory='keras_tuner_dir',
+            project_name='MotorPlusMPG'
+        )
+        early_stop = callbacks.EarlyStopping(monitor='val_loss', patience=5)
+        tuner.search(self.X_train, self.y_train, epochs=40, batch_size=32, validation_data=(self.X_test, self.y_test), callbacks=[early_stop])
+        best_hps = tuner.get_best_hyperparameters(num_trials=1)[0]
+        self.build_model(best_hps)
+        self.train_model()
+        self.evaluate_model()
+        self.plot_history()
+        self.save_model()
+        
 
-def build_model(input_shape):
-    model = Sequential()
-    model.add(Dense(8, activation='relu', input_shape=(input_shape.shape[1],)))
-    model.add(Dense(8, activation='relu'))
-    model.add(Dense(1, activation='linear'))
-    adam = optimizers.Adam(learning_rate=0.01)
-    model.compile(optimizer=adam, loss='mean_squared_error', metrics=['accuracy'])
-    return model
-
-def train_model(model, X_train, y_train):
-    history = model.fit(X_train, y_train, epochs=40, batch_size=32, callbacks=[tensorboard], verbose=0)
-    return history
-
-def evaluate_model(model, X_test, y_test):
-    test_loss = model.evaluate(X_test, y_test, verbose=0)
-    return test_loss
-
-def predict(df):
-    return model.predict(df)
-
-def plot_history(history):
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['accuracy'])
-    plt.title('Model Loss and Accuracy')
-    plt.ylabel('Loss / Accuracy')
-    plt.xlabel('Epoch')
-    plt.legend(['Loss', 'Accuracy'], loc='upper right')
-    plt.show()
-
-def save_model(model):
-    if not os.path.exists('models_qa'):
-        os.makedirs('models_qa')
-    time_str = datetime.datetime.now().strftime('%Y_%m_%d_%H_%M')
-    model.save(f"models_qa/model_keras_latest.h5")
-
-def load_model(model_file):
-    return keras.models.load_model(model_file)
-
-if __name__ == "__main__":
-    filepath = 'Datasets/auto-mpg.csv'
-    target_column = 'mpg'
-    categorical_features = ['origin']
-    df = load_data(filepath)
-    df = one_hot_encode(df, categorical_features)
-    X_train, X_test, y_train, y_test = split_data(df, target_column)
-    X_train, X_test = standardise_data(X_train, X_test)
-    model = build_model(X_train)
-    plot_model(model, to_file='model.png', show_shapes=True, show_layer_names=True)
-    tensorboard = TensorBoard(log_dir='./logs', histogram_freq=0, write_graph=True, write_images=True)
-    history = train_model(model, X_train, y_train)
-    test_loss = evaluate_model(model, X_test, y_test)
-    plot_history(history)
-    save_model(model)
+if __name__ == '__main__':
+    nn = Keras_NN('Datasets/auto-mpg.csv', target_column='mpg', categorical_features=['origin'])
+    nn.run()
--- classifier_testing.py
+++ classifier_testing.py
@@ -1,34 +1,27 @@
 import unittest
 import pandas as pd
 import numpy as np
-from NeuralNetwork import NeuralNetwork
+import keras
+from keras.models import load_model
+from sklearn.preprocessing import StandardScaler
 
-class TestClassifier(unittest.TestCase):
+class TestKerasModel(unittest.TestCase):
     def setUp(self):
-        # Load the test data
-        data = pd.read_csv("vehicles.csv")
+        self.model_file = 'models_qa/model_keras_latest.h5'
+        self.df = pd.read_csv('Datasets/auto-mpg.csv', na_values="?")
+        self.df.dropna(inplace=True)
+        self.df.drop(columns=['car name'], inplace=True)
+        self.df = pd.get_dummies(self.df, columns=['origin'])
+        self.scaler = StandardScaler()
+        self.scaler.fit(self.df.drop('mpg', axis=1))
 
-        # Extract the input and output data
-        X = data.drop("combination_mpg", axis=1)
-        y = data[["combination_mpg"]]
-
-        # Preprocess the data
-        X = pd.get_dummies(X)
-        X = (X - X.mean()) / X.std()
-
-        self.X_test = X.values
-        self.y_test = y.values
-
-    def test_predict(self):
-        # Load the trained model
-        filename = "models/model_2023-01-22-17-30.pkl"
-        nn = NeuralNetwork.load(filename)
-
-        # Use the predict function to make predictions
-        y_pred = nn.predict(self.X_test)
-
-        # Check if the predictions are close to the actual values
-        np.testing.assert_allclose(y_pred, self.y_test, rtol=1e-05, atol=1e-08)
+    def test_model_predictions(self):
+        model = load_model(self.model_file)
+        X_test = self.scaler.transform(self.df.drop('mpg', axis=1).iloc[:10, :])
+        y_test = self.df['mpg'].iloc[:10]
+        predictions = model.predict(X_test)
+        for i in range(len(predictions)):
+            self.assertAlmostEqual(predictions[i][0], y_test.iloc[i], delta=0.1)
 
 if __name__ == '__main__':
     unittest.main()
